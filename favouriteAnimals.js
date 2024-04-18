066bf5fb317ee19ea1becc17cb077d5f9b8d86e4
RAbeyrathna <rahal@res-network.com>
2024-04-18 20:43:36 +1000

    feat: Add reveal and hide feature to input hints

--- favouriteAnimals.js
+++ favouriteAnimals.js
@@ -64,25 +64,56 @@ function removeAnimalFromList(targetAnimalId){
 
 
 function addAnimalToList(event, targetInputId){
-    // Disable form default event of real form
+    // Disable default event of real form
     event.preventDefault();
 
     // 1. Find the input field matching targetInputId
-    let targetInputField = document.getElementById(targetInputId);
+    let targetInputField = document.querySelector(targetInputId);
 
     // 2. Get the text value from the input field that we found 
     let foundInputFieldValue = targetInputField.value;
     console.log("Input field value to add to list is: " + foundInputFieldValue);
 
+    //2.5 Validation - Check if input is empty
+    if(!foundInputFieldValue){
+        alert("Empty value, not adding to the list")
+        return;
+    }
+
     // 3. Push the text value into the animals array 
     animals.push(foundInputFieldValue);
 
+    // 3.5 Reset input field
+    targetInputField.value = "";
+
     // 4. Recreate the animal list
     createAnimalList();
+    
 }
 
-let fakeFormButton = document.getElementById("fakeform-submit");
-let realFormButton = document.getElementById("realform-submit");
+let fakeFormButton = document.querySelector("#fakeform-submit");
+let realFormButton = document.querySelector("#realform-submit");
 
-fakeFormButton.addEventListener("click", (event) => {addAnimalToList(event, "fakeform-addAnimal")});
-realFormButton.addEventListener("click", (event) => {addAnimalToList(event, "realform-addAnimal")});
+fakeFormButton.addEventListener("click", (event) => {addAnimalToList(event, "#fakeform-addAnimal")});
+realFormButton.addEventListener("click", (event) => {addAnimalToList(event, "#realform-addAnimal")});
+
+
+function inputHelperReveal(targetElementId){
+    let hintElement = document.querySelector(targetElementId);
+    hintElement.style.display = "inherit";
+}
+function inputHelperHide(targetElementId){
+    let hintElement = document.querySelector(targetElementId);
+    hintElement.style.display = "none";
+}
+
+let realFormInput = document.getElementById("realform-addAnimal");
+realFormInput.addEventListener("focusin", () => {inputHelperReveal("#realform-hint")});
+realFormInput.addEventListener("focusout", () => {inputHelperHide("#realform-hint")});
+
+let fakeFormInput = document.getElementById("fakeform-addAnimal");
+fakeFormInput.addEventListener("focusin", () => {inputHelperReveal("#fakeform-hint")});
+fakeFormInput.addEventListener("focusout", () => {inputHelperHide("#fakeform-hint")});
+
+inputHelperHide("realform-hint");
+inputHelperHide("fakeform-hint");
